f7d1858d40bdd6f3380d6fd0b2cbc085bfd3c580
Štěpán Záliš <zalis@damidev.com>
2019-08-21 12:53:07 +0200

    REFACTOR: widget is now jumping when text size changed

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -7,15 +7,14 @@ import 'package:light_sensor/utils/constants.dart';
 import 'package:light_sensor/widgets/text_widget.dart';
 
 class HomePage extends StatefulWidget {
-
   HomePage({Key key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
-
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   StreamSubscription _lightSubscription;
 
   final _lightBloc = LightBloc();
@@ -27,15 +26,25 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
         body: StreamBuilder<double>(
             initialData: _lightBloc.lightValue,
             stream: _lightBloc.lightStream,
-            builder: (context, snapshot) => TextWidgetContainer(luxValue: snapshot.data)));
+            builder: (context, snapshot) => Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  color: Color.lerp(
+                      Colors.black, Colors.white, _computeColor(snapshot.data)),
+                  child: LightTextContainer(
+                      lightValue: snapshot.data, bloc: _lightBloc),
+                )));
   }
 
-  _setLightValue(double value) async => _lightBloc.lightSink.add(value);
+  _setLightValue(double value) => _lightBloc.lightSink.add(value);
+  _computeColor(double value) => value / 10;
 
   @override
   void initState() {
     super.initState();
-    _lightSubscription = streamChannel.receiveBroadcastStream().listen((data) => _setLightValue(data));
+    _lightSubscription = streamChannel
+        .receiveBroadcastStream()
+        .listen((data) => _setLightValue(data));
   }
 
   @override
@@ -45,3 +54,4 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
     _lightSubscription.cancel();
   }
 }
+
--- lib/widgets/text_widget.dart
+++ lib/widgets/text_widget.dart
@@ -1,32 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:light_sensor/light_bloc.dart';
 
-class TextWidgetContainer extends StatelessWidget {
+class LightTextContainer extends StatelessWidget {
 
-  final double luxValue;
-  const TextWidgetContainer({@required this.luxValue});
+  final double lightValue;
+  final LightBloc bloc;
+
+  const LightTextContainer({
+    @required this.lightValue,
+    @required this.bloc,
+    Key key,
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container(
-        width: double.infinity,
-        height: double.infinity,
-        color: Color.lerp(Colors.black, Colors.white, _computeColor(luxValue)),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Center(child: Text("Lux value from sensor", style: TextStyle(color: Colors.grey[300], fontSize: 20))),
-            SizedBox(height: 15),
-            Center(
-                child: Text("$luxValue",
-                    style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: _computeFontSize(luxValue),
-                        fontFamily: DefaultTextStyle.of(context).style.fontFamily)))
-          ],
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          width: screenWidth,
+          top: screenHeight / 2 - 50,
+          child: Text("Light value from sensor",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.amber[600],
+                  fontSize: 20,
+                  fontFamily: DefaultTextStyle.of(context).style.fontFamily)),
         ),
-      );
+        Positioned(
+          top: screenHeight / 2,
+          width: screenWidth,
+          child: Text("$lightValue",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: _computeFontSize(lightValue),
+                  fontFamily: DefaultTextStyle.of(context).style.fontFamily)),
+        )
+      ],
+    );
+  }
 
   _computeFontSize(double value) => value >= 40 ? 40.0 : value * 3;
-
-  _computeColor(double value) => value / 10;
 }
